50645062a71a039da2f21df90942e33a9e287f70
Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>
2025-02-04 02:57:42 +0100

    Added genome (#3)

--- DarwinSimulator/DarwinSimulator/model/Genome.cs
+++ DarwinSimulator/DarwinSimulator/model/Genome.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace DarwinSimulator.model
         protected readonly Random rand = new Random();
         protected int[] genes;
         protected readonly int genomeLength;
+        protected readonly int geneMaxVal;
         protected int currentIndex;
 
         public Genome(Parameters parameters)
@@ -21,8 +23,28 @@ namespace DarwinSimulator.model
             genomeLength = parameters.GenomeParameters.GenomeLength;
             genes = new int[genomeLength];
             currentIndex = rand.Next(0, genomeLength);
+            geneMaxVal = MapDirectionExtension.GetLength();
 
-            genes.Select(x => x = rand.Next(MapDirectionExtension.GetLength() + 1));
+            genes.Select(x => x = rand.Next(geneMaxVal + 1));
+        }
+
+        public Genome(Genome left, Genome right, int leftEnergy, int rightEnergy, Parameters parameters)
+        {
+            Parameters = parameters;
+            genomeLength = parameters.GenomeParameters.GenomeLength;
+            genes = new int[genomeLength];
+            currentIndex = rand.Next(genomeLength);
+            geneMaxVal = MapDirectionExtension.GetLength();
+
+            int leftLength = genomeLength * leftEnergy / (leftEnergy + rightEnergy);
+            int rightLength = genomeLength - leftLength;
+
+            if (rand.Next(2) == 0)
+                genes = left.genes.Take(leftLength).Concat(right.genes.Skip(leftLength)).ToArray();
+            else
+                genes = right.genes.Take(rightLength).Concat(left.genes.Skip(rightEnergy)).ToArray();
+
+            Mutate();
         }
 
         public int GetNext()
---
+++ DarwinSimulator/DarwinSimulator/model/GenomeFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinSimulator.model
+{
+    internal class GenomeFactory
+    {
+        public static Genome createGenome(Parameters parameters)
+        {
+            switch (parameters.GenomeParameters.GenomeType)
+            {
+                case GenomeType.NORMAL_GENOME:
+                    return new NormalGenome(parameters);
+                case GenomeType.SWAPPING_GENOME:
+                    return new SwappingGenome(parameters);
+                case GenomeType.SLIGHTLY_ADJUSTING_GENOME:
+                    return new SlightlyAdjustingGenome(parameters);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        public static Genome createGenome(Genome left, Genome right, int leftEnergy, int rightEnergy, Parameters parameters)
+        {
+            switch (parameters.GenomeParameters.GenomeType)
+            {
+                case GenomeType.NORMAL_GENOME:
+                    return new NormalGenome(left, right, leftEnergy, rightEnergy, parameters);
+                case GenomeType.SWAPPING_GENOME:
+                    return new SwappingGenome(left, right, leftEnergy, rightEnergy, parameters);
+                case GenomeType.SLIGHTLY_ADJUSTING_GENOME:
+                    return new SlightlyAdjustingGenome(left, right, leftEnergy, rightEnergy, parameters);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
