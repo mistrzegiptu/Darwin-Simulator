adedd8e13c7538643cc567b6d174b979a0215831
Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>
2025-02-22 22:52:12 +0100

    Added PoleMap and started working on FireMap (#9)

---
+++ DarwinSimulator/DarwinSimulator/model/Fire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinSimulator.model
+{
+    internal class Fire : IWorldElement
+    {
+        public Vector2d Position { get; }
+        public int DaysActive { get; }
+
+        public Fire(Vector2d position)
+        {
+            Position = position;
+        }
+
+        public override string ToString()
+        {
+            return "F";
+        }
+    }
+}
