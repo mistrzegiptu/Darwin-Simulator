6d3d5b84b8cee661009340d0702157f90aa7f0cb
Franek Jawor <39954561+mistrzegiptu@users.noreply.github.com>
2025-02-25 03:35:19 +0100

    Finished firemap (#10)

--- DarwinSimulator/DarwinSimulator/model/FireMap.cs
+++ DarwinSimulator/DarwinSimulator/model/FireMap.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace DarwinSimulator.model
 {
-    internal class FireMap
+    internal class FireMap : WorldMap
     {
+        private Dictionary<Vector2d, IWorldElement> _fires = new();
+
+        public FireMap(Parameters parameters) : base(parameters)
+        {
+        }
+
+        public override void PassDay(int day)
+        {
+            if (day % parameters.WorldParameters.NewFirePeriod == 0)
+                CreateFire();
+            SpreadOrExtinguish();
+            base.PassDay(day);
+        }
+
+        public void CreateFire()
+        {
+            Vector2d newFirePosition = plants.Keys.First();
+
+            _fires.Add(newFirePosition, new Fire(newFirePosition));
+            plants.Remove(newFirePosition);
+        }
+
+        public void SpreadOrExtinguish()
+        {
+            foreach(Fire fire in _fires.Values)
+            {
+                if (fire.DaysActive == parameters.WorldParameters.FireDuration)
+                    _fires.Remove(fire.Position);
+                else
+                {
+                    fire.AddActiveDay();
+
+                    if (animals.ContainsKey(fire.Position))
+                        animals[fire.Position].ForEach(animal => animal.Die());
+
+                    foreach (var direction in MapDirectionExtension.GetMainDirections())
+                    {
+                        Vector2d spreadingPosition = fire.Position.Add(direction.ToUnitVector());
+                        if (plants.ContainsKey(spreadingPosition))
+                        {
+                            _fires.Add(spreadingPosition, new Fire(spreadingPosition));
+                            plants.Remove(spreadingPosition);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
